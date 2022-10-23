b028f7bfbc52e70a38e57cb2285444ba40a58353
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-23 12:30:13 +0500

    sakkiz0808

--- String08.py
+++ String08.py
@@ -7,5 +7,5 @@ def main(first,last):
     Returns:
         str: return answer.
     """
-    return  last + ','+first
-print(main("Jasurbek", "Pirnazarov"))
+    return last+', '+first
+print(main('"Jasurbek', '"Pirnazarov"'))
