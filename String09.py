064b0734ae7e476ee4d45d260909111f71f5fbaf
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-24 13:48:17 +0500

    string

--- String09.py
+++ String09.py
@@ -9,7 +9,7 @@ def main(x1,x2,x3):
         str: return answer.
     """
    
-    ans ='"['+str(x1)+", "+str(x2)+", "+str(x3)+']"'
+    ans = "'["+str(x1)+", "+str(x2)+", "+str(x3)+"]'"
     return ans
 print(main(1,2,4))
 
